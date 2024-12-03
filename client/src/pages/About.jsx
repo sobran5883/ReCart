558f1163ffd91954e9621789ac23ba6b30616d42
Sobran Rajak <sobran0621@gmail.com>
2024-12-04 03:30:48 +0530

    recart first commit

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,16 @@
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About ReCart</h1>
+      <p className='mb-4 text-slate-700'>
+        ReCart is a trusted online marketplace for buying and selling quality second-hand products. We aim to create a sustainable and accessible platform where users can find great deals on pre-owned items, while also providing a simple and secure way to sell items they no longer need.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Our mission at ReCart is to empower users to make environmentally-conscious choices by promoting the reuse of quality products. Whether you are looking to buy pre-owned electronics, furniture, clothing, or household items, or you want to give a second life to items you no longer use, ReCart is here to help you achieve your goals.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Our team is committed to providing a safe, user-friendly platform that makes buying and selling second-hand items enjoyable and hassle-free. We believe that finding value in used products should be both rewarding and convenient, and we are dedicated to supporting our community of buyers and sellers in every way we can.
+      </p>
+    </div>
+  );
+}
